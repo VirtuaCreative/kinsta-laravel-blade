947097557ffa60a8f407d62b457eb16374aa2f41
Marcia Ramos <13464393+VirtuaCreative@users.noreply.github.com>
2023-07-26 15:04:09 +0100

    Update files with the tutorial steps

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\DB; // Import DB Facade
 use Illuminate\Support\Facades\Route;
 
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('welcome');
+    $todos = ['Learn Laravel', 'Learn Blade', 'Build Cool Stuff'];
+    return view('welcome', ['todos' => $todos]);
 });
 
-Route::get('/new', function () {
-    // $todos = DB::table('todos')->get();
-    $todos = ['Learn Laravel', 'Learn Blade', 'Build Cool Stuffs'];
-    return view('child', ['todos' => $todos]);
-});
